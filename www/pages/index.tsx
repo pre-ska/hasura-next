fb19f573743bdd96bdb99f76210a1a4ae1a1e9fd
preska <wolfwar@outlook.com>
2022-11-20 23:00:47 +0100

    utils libs urql graphql-ws ssr prepass

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -66,7 +66,7 @@ const Home: NextPage = ({
         ))}
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -77,7 +77,7 @@ const Home: NextPage = ({
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 };
